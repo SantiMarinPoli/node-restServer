cd900c1b3b06bc8157a9cb5315ccc3129597f5d5
sala 208 <sala208@poliacademico.edu.loc>
2019-07-17 17:26:27 -0500

    Variable el entorno

--- server/config/config.js
+++ server/config/config.js
@@ -11,12 +11,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 // SI UNA PERSONA SE CONECTA CON EL MODO DE DESARROLLO SE 
 // ENVIARA LA RUTA DEL LOCAL DE MONGO O SI NO TRABAJARA 
 // EN LA NUBE DEL MONGO ATLAS
+//MONGO_URL => hemos creado una variable del entorno en heroku para ocultar la ruta de la nube de mongo
 let urlDB;
 
   if(process.env.NODE_ENV == 'dev'){
       urlDB = 'mongodb://localhost:27017/cafe'
   }else{
-    urlDB =' mongodb+srv://santi:jfORbN7YCpNgGWl5@cluster0-0syrz.mongodb.net/cafe'
+    urlDB =process.env.MONGO_URL
   }
 
 process.env.URLDB = urlDB
