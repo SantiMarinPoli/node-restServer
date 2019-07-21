38030b8cc165d3747b0f7ef9475952fbea237969
Santy <santi.lamarin98@gmail.com>
2019-07-20 19:43:05 -0500

    proyecto hasta ahora

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 let rolesValidos = {
-    values: ['ADMIN_ROLE','USER_ROLE'],
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
     message: '{VALUE} no es un rol valido'
 }
 
@@ -11,49 +11,62 @@ let Schema = mongoose.Schema;
 
 //ES EL "CONSTRUCTOR " DECLARA LOS ATRIBUTOS DE LA TABLA USUARIO EN EL MONGODB
 let usuarioSchema = new Schema({
-    nombre:{
+    nombre: {
         type: String,
-        required: [true,'El nombre es necesario']
+        required: [true, 'El nombre es necesario']
     },
 
-    email:{
+    apellido: {
+        type: String,
+        required: [true, 'El apellido es necesario']
+    },
+
+    email: {
         type: String,
         unique: true,
-        required: [true,'El correo es necesario']
+        required: [true, 'El correo es necesario']
     },
 
-    password:{
+    password: {
         type: String,
-        required: [true,'El password es necesario']
+        required: [true, 'El password es necesario']
     },
 
-    img:{
+    repitaPassword: {
+        type: String,
+        required: [true, 'El repita password es necesario']
+    },
+
+    telefono: {
+        type: String,
+        required: [true, 'El telefono es necesario']
+    },
+
+    img: {
         type: String,
         required: false
     },
 
-    role:{
+    edad: {
         type: String,
-        default: 'USER_ROLE',
+        required: false
+    },
+
+    role: {
+        type: String,
         enum: rolesValidos
     },
 
-    estados:{
+    estados: {
         type: Boolean,
         default: true
 
     },
-
-    google:{
-        type: Boolean,
-        default: false,
-
-    }
 });
 
 //ESTA FUNCION NO MUESTRA EL ATRIBUTO PASSWORD PARA TENER UNA MAYOR SGURIDAD DE LA APLICACION
 //CUANDO GUARDA LAS BASES DE DATOS MOSTRARA EL DATO ENCRIPTADO
-usuarioSchema.methods.toJSON = function(){
+usuarioSchema.methods.toJSON = function() {
 
     let users = this
     let userObject = users.toObject()
@@ -63,7 +76,7 @@ usuarioSchema.methods.toJSON = function(){
 }
 
 //UNIQUE VALIDATOR UTILIZA SOBRE VALIDAR LOS DATOS SON UNICOS ES DECIR NO DEBE REPETIRSE
-usuarioSchema.plugin(uniqueValidator,{message:'{PATH} debe ser unico'})
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' })
 
 //EXPOTAR EL MODULO EXPORT PARA QUE CREA AUTOMATICAMENTE LA CLASE USUARIO EN MONGODB
-module.exports = mongoose.model('Usuario',usuarioSchema)
+module.exports = mongoose.model('Usuario', usuarioSchema)
--- server/route/user.js
+++ server/route/user.js
@@ -8,7 +8,7 @@ const _ = require('underscore')
 const app = express()
 
 //POST MUESTRA DATOS O LISTAR DATOS
-app.get('/user', function (req, res) {
+app.get('/user', function(req, res) {
 
     //sIGNAR UNA NUEVA VARIABLE QUE PUEDA REALIZAR UN FILTRO OPCIONAL ES DECIR
     //desde el usuario quiere buscar un nombre or sino muestre los n de datos
@@ -18,7 +18,7 @@ app.get('/user', function (req, res) {
     //user?desde=n
     // si el usuario quiere filtrar mas de dos parametros 
     //son el siguiente ejemplo
-    //user?desde=n&limite=n
+    //user?desde=n&limite=n.
     let desde = req.query.desde || 0;
     desde = Number(desde)
 
@@ -31,132 +31,108 @@ app.get('/user', function (req, res) {
     //exec(err,param) su funcion es ejecutelo 
     //skip(n) su funcion que salte n registros
     //limit(n) su funcion es mostrar n de registros
-    Usuario.find({estados:true},'nombre email role estados google img')
-            .skip(desde)
-            .limit(limite)
-            .exec((err,usuarios)=>{
-              if(err){
+    Usuario.find({ estados: true }, 'nombre apellido email edad telefono role estados img ')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
                 return res.status(400).json({
-                  ok: false,
-                  err
+                    ok: false,
+                    err
                 })
-              }
+            }
 
-              //count() contar el numero de registros almacenados
-              Usuario.count({estados:true},(err,conteo)=>{
-                  res.json({
-                    ok:true,
+            //count() contar el numero de registros almacenados
+            Usuario.count({ estados: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
                     usuarios,
                     cuantos: conteo
-                  })
-
-              })
-
+                })
 
             })
-    
-  })
-  
-  //POST UTILIZAN SOLO REGISTROS
-  app.post('/user', function (req, res) {
-  
-      let body = req.body
-
-      //ESTE OBJETO MOSTARA LOS DATOS QUE SON REQUISITOS PARA COMPLETAR LOS DATOS
-      let usuario = new Usuario({
-          nombre: body.nombre,
-          email: body.email,
-          password: bcrypt.hashSync(body.password,10),
-          role: body.role
-      })
-
-      //CUANDO LA CONECCION DEL MONGO NO SE CONECTA ARROJARA UN MENSAJE DE ERROR EN LA CONSOLA
-      usuario.save((err,usuarioDB)=>{
-          if(err){
-                return res.status(400).json({
-                  ok: false,
-                  err
-                })
-          }
-
-          res.json({
-              ok: true,
-              usuario: usuarioDB
-          })
-      })
-    })
-  
-  //PUT UTILIZAN DE ACTUALIZAR LOS REGISTROS
-  app.put('/user/:id', function (req, res) {
-      let id = req.params.id
-      let body = _.pick(req.body,['nombre','email','img','role','estados']);
 
 
-      
-
-      //{new: true} es un objeto, el metodo esta la documentacion de mongoose
-      // la funcion del new es mostrar el dato antes de modificar el dato
-
-      //runValidators esta la documentacion de mongoose
-      //la funcion es validar los datos que usted han asignado en la carpeta models/modelos
-      Usuario.findByIdAndUpdate(id, body,{new:true, runValidators: true}, (err, usuarioDB)=>{
-        
-        if(err){
-          return res.status(400).json({
-            ok: false,
-            err
-          })
-        }
 
 
-        res.json({
-              ok: true,
-              usuario : usuarioDB
         })
 
 
-      })
-      
+})
 
+//POST UTILIZAN SOLO REGISTROS
+app.post('/user', function(req, res) {
+
+
+
+    let body = req.body
+
+    // //ESTE OBJETO MOSTARA LOS DATOS QUE SON REQUISITOS PARA COMPLETAR LOS DATOS
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        apellido: body.apellido,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        repitaPassword: bcrypt.hashSync(body.repitaPassword, 10),
+        telefono: body.telefono,
+        edad: body.edad,
+        role: body.role
     })
-  
-  //DELETE
-  app.delete('/user/:id', function (req, res) {
 
-      let id = req.params.id;
-      let cambiaEstados ={
-        estados: false
-      }
-      //findByIdUpdate 
-      // la funcion es una accion que pueda borrar un dato es lo mismo la funcion del update
-      // Usuario.findByIdAndRemove(id,(err,usuarioBorrado)=>{
-        Usuario.findByIdAndUpdate(id,cambiaEstados,{new:true},(err,usuarioBorrado)=>{
-          if(err){
+    // //CUANDO LA CONECCION DEL MONGO NO SE CONECTA ARROJARA UN MENSAJE DE ERROR EN LA CONSOLA
+    usuario.save((err, usuarioDB) => {
+        if (err) {
             return res.status(400).json({
-              ok: false,
-              err
+                ok: false,
+                err
             })
-          }
+        }
 
-          //sino existe el dato lanzara un error en la consola
-          if(!usuarioBorrado){
-            return res.status(400).json({
-              ok: false,
-              error: {
-                message: 'Usuario no encontrado'
-              }
-            })
-          }
-
-          //este codigo borra el dato fisico
-          res.json({
+        res.json({
             ok: true,
-            usuario: usuarioBorrado
-          })
+            usuario: usuarioDB
+        })
+    })
+})
 
+//PUT UTILIZAN DE ACTUALIZAR LOS REGISTROS 123
+app.put('/user/:id', function(req, res) {
+    let id = req.params.id
+    let body = req.body
+        // let body = _.pick(req.body, ['nombre', 'apellido', 'email', 'password', 'repitaPassword', 'img', 'edad', 'telefono', 'estados']);
 
-      })
+    Usuario.findByIdAndUpdate(id, body, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
 
     })
+
+
+    // //{new: true} es un objeto, el metodo esta la documentacion de mongoose
+    // // la funcion del new es mostrar el dato antes de modificar el dato
+
+    // //runValidators esta la documentacion de mongoose
+    // //la funcion es validar los datos que usted han asignado en la carpeta models/modelos
+    // Usuario.findByIdAndUpdate(id, body,{new:true, runValidators: true}, (err, usuarioDB)=>{
+
+
+
+
+
+
+
+})
+
+
 
 module.exports = app
